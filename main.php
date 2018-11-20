b68c4fff96bb681045ec215d42f5bc541625a100
Artem.Panche <Ymrtpanche@gmail.com>
2018-11-21 01:53:43 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>«Тест: индивидуальное потребление калорий» </title>
+<link href="reset.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		
+		<h2>«Тест: индивидуальное потребление калорий»</h2>
+		<div class="ContainerText">
+			<p>
+			   Калори́йность (от калории — старой единицы измерения энергии): <br>
+			   Энергетическая ценность пищи — количество энергии, получаемой организмом<br>
+			   из продукта питания. Теплота сгорания — количество энергии, выделяющейся<br>
+			   при полном сгорании вещества.
+			</p>   
+		<form method="GET" action="handler.php"> <!--указание метода GET-->
+			Login: <input type="text" name="login">
+			E-mail: <input type="text" name="email">
+			<a href="handler.php"><input type="submit" value="Отправить"></a>
+		</form>
+		</div>	
+	</div>		
+</body>
+</html>
